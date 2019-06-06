149cb1ae8c8b15ca26d1f169c498e2ac3b88850d
uday <udayakumarl@deemsysinc.com>
2019-06-06 19:41:21 +0530

    New project code commit initial

---
+++ ios/NellaiMarts/NellaiMarts/Launch.swift
@@ -0,0 +1,83 @@
+//
+//  Launch.swift
+//  Shoppickk
+//
+//  Created by admin on 04/06/19.
+//  Copyright © 2019 Deemsys. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Reachability
+import Alamofire
+import SwiftyJSON
+
+
+class Launch: UIViewController {
+    
+    let fileUrl:String = "https://mobilewebapps.s3.amazonaws.com/nellaimarts.json"
+    let reachability = Reachability()!
+    
+    @IBOutlet weak var Indicator: UIActivityIndicatorView!
+    override func viewDidLoad() {
+        // page onload
+        self.Indicator.startAnimating()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(reachabilityChanged(note:)), name: .reachabilityChanged, object: reachability)
+        do{
+            try reachability.startNotifier()
+        }catch{
+            print("could not start reachability notifier")
+        }
+    }
+    
+    
+    
+    @objc func reachabilityChanged(note:Notification) {
+        let reachability = note.object as! Reachability
+        switch reachability.connection {
+        case .cellular:
+            //cellular connection received
+            print("cellular connection")
+            self.loadData()
+        case .wifi:
+            // wifi connection received
+            print("Wifi connection")
+             self.loadData()
+        case .none:
+            // no connection
+            print("No connection")
+            let alertController = UIAlertController(title: "No Internet ☹", message: "Please check your internet connection.", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Close", style: .cancel, handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    override func viewWillDisappear(_ animated:Bool) {
+        super.viewWillAppear(animated)
+        reachability.stopNotifier()
+        NotificationCenter.default.removeObserver(self, name: .reachabilityChanged, object: reachability)
+    }
+    
+    func loadData(){
+        Alamofire.request(self.fileUrl).responseData { (responseData) in
+            //print(responseData.result.value as Any)
+            if responseData.result.value != nil{
+                if let response = try? JSON(data: responseData.result.value!, options: .mutableContainers){
+                    self.Indicator.stopAnimating()
+                    let dict = response.dictionaryObject as! [String:String]
+                    let defaults = UserDefaults.standard
+                    defaults.setValuesForKeys(dict)
+                    defaults.synchronize()
+                    // Navigate
+                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                    let VC = storyboard.instantiateViewController(withIdentifier: "main") as! ViewController
+                    self.navigationController?.pushViewController(VC, animated: true)
+                }
+            }
+        }
+    }
+}
---
+++ ios/NellaiMarts/NellaiMarts/ViewController.swift
@@ -0,0 +1,129 @@
+//
+//  ViewController.swift
+//  Shoppickk
+//
+//  Created by admin on 04/06/19.
+//  Copyright © 2019 Deemsys. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+import WebKit
+import Reachability
+
+
+
+class ViewController: UIViewController,MFMailComposeViewControllerDelegate, WKNavigationDelegate,UIWebViewDelegate,UIScrollViewDelegate {
+    var defaults:UserDefaults!
+    
+    
+
+    @IBOutlet weak var progress: UIProgressView!
+    @IBOutlet weak var webKitView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 47.0/255.0, green: 199.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: UIFont(name: "Avenir Next", size: 21.0)!]
+        self.navigationItem.title = "Nellai Marts Inc"
+        self.navigationController?.navigationBar.tintColor = .white
+        
+        
+        let shareBarButton = UIBarButtonItem(image: #imageLiteral(resourceName: "Share"), style: .plain, target: self, action: #selector(Share))
+        let callBarButton = UIBarButtonItem(image: #imageLiteral(resourceName: "Call"), style: .plain, target: self, action: #selector(Call))
+        let emailBarButton = UIBarButtonItem(image: #imageLiteral(resourceName: "Email"), style: .plain, target: self, action: #selector(Message))
+        self.navigationItem.setRightBarButtonItems([emailBarButton,callBarButton], animated: true)
+        let scanBarButton = UIBarButtonItem(image: #imageLiteral(resourceName: "barcode"), style: .plain, target: self, action: #selector(Scan))
+        self.navigationItem.setLeftBarButtonItems([scanBarButton,shareBarButton], animated: true)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.defaults = UserDefaults.standard
+        self.webKitView.navigationDelegate = self
+        self.webKitView.scrollView.delegate = self
+        let reachability = Reachability()!
+        if reachability.connection != .none{
+            self.LoadSite()
+        }else{
+            self.displayNetworkAlert()
+        }
+        webKitView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+            if keyPath == "estimatedProgress"{
+                //print (Float(webKitView.estimatedProgress))
+                self.progress.progress = Float(webKitView.estimatedProgress)
+            }
+    }
+    
+    func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+        scrollView.pinchGestureRecognizer?.isEnabled = false
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let reachability = Reachability()!
+        if reachability.connection != .none{
+            decisionHandler(.allow)
+            return
+        }
+        self.displayNetworkAlert()
+        decisionHandler(.cancel)
+    }
+    
+    func LoadSite(){
+        print(defaults!)
+        if let url = URL(string: defaults.string(forKey: "websiteUrl")!){
+            let request = URLRequest(url: url)
+            webKitView.load(request)
+        }
+    }
+    
+    @objc func Call(){
+        if let url = NSURL(string: "tel://\(defaults.string(forKey: "call")!)"), UIApplication.shared.canOpenURL(url as URL) {
+            UIApplication.shared.open(url as URL)
+        }
+    }
+    
+    @objc func Message(){
+        if !MFMailComposeViewController.canSendMail(){
+          print ("Mail service not available")
+            return
+        }
+        let composer = MFMailComposeViewController()
+        composer.mailComposeDelegate = self
+        composer.setToRecipients([defaults.string(forKey: "mail")!])
+        self.present(composer, animated: true, completion: nil)
+        
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?){
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func Share(){
+        let shareContent = "\(defaults.string(forKey: "share")!) \(" ") \(defaults.string(forKey: "iosRateusUrl")!)"
+        let activityController = UIActivityViewController(activityItems: [shareContent], applicationActivities: [])
+        self.present(activityController, animated: true, completion: nil)
+    }
+    /*
+    @objc func Rate(){
+        if let url = NSURL(string: defaults.string(forKey: "iosRateusUrl")!), UIApplication.shared.canOpenURL(url as URL) {
+            UIApplication.shared.open(url as URL)
+        }
+    }*/
+    
+    @objc func Scan(){
+        // Bar code scanning
+    }
+    
+    func displayNetworkAlert() {
+        let alertController = UIAlertController(title: "No Internet ☹", message: "Please check your internet connection.", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Close", style: .cancel, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+
+}
+
